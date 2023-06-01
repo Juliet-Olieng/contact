3d48a4bf06bddd0230b025e3538aae1b6f8bcffe
Juliet-Olieng <anyangojolieng@gmail.com>
2023-06-01 08:42:45 +0300

    committing changes in the app

--- app/src/main/java/com/juliet/contactapk/MainActivity.kt
+++ app/src/main/java/com/juliet/contactapk/MainActivity.kt
@@ -22,11 +22,14 @@ class MainActivity : AppCompatActivity() {
 
 
     fun displayContact() {
-        var contact1 = ContactData("daddy", "nicholas@gmail.com", "+254723469938")
-        var contact2 = ContactData("phelix", "phaliza@gmail.com", "+254703728432")
-        var contact3 = ContactData("dester", "dester@gmail.com", "+254703924627")
-        var contact4 = ContactData("juliet", "juliet@gmail.com", "+254718492299")
-        var contactlists = listOf<ContactData>(contact1, contact2, contact3, contact4)
+        var contact1 = ContactData("daddy", "nicholas@gmail.com", "+254723469938","")
+        var contact2 = ContactData("phelix", "phaliza@gmail.com", "+254703728432","")
+        var contact3 = ContactData("dester", "dester@gmail.com", "+254703924627","")
+        var contact4 = ContactData("juliet", "juliet@gmail.com", "+254718492299","")
+        var contact5 = ContactData("teves", "teves@gmail.com", "+254718493474","")
+        var contact6 = ContactData("tamara", "tamara@gmail.com", "+254718494523","")
+        var contact7 = ContactData("wein", "wein@gmail.com", "+254718497733","")
+        var contactlists = listOf<ContactData>(contact1, contact2, contact3, contact4,contact5,contact6,contact7)
         binding.rvContact.layoutManager = LinearLayoutManager(this)
         var contactAdapter = ContactRvAdapter(contactlists)
         binding.rvContact.adapter = contactAdapter
--- app/src/main/java/com/juliet/contactapk/contactData.kt
+++ app/src/main/java/com/juliet/contactapk/contactData.kt
@@ -3,6 +3,7 @@ package com.juliet.contactapk
 data class ContactData(
     var name:String,
     var emailAddress:String,
-    var phoneNumber:String
+    var phoneNumber:String,
+    var avatar:String
 
 )
